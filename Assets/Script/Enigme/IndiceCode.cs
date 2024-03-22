898a1777cebe8ee52cbbcd9a930ea45375095019
Eva291204 <eva.fricaud@e-artsup.net>
2024-03-22 09:03:28 +0100

    cadenas couleur

--- Assets/Script/Enigme/IndiceCode.cs
+++ Assets/Script/Enigme/IndiceCode.cs
@@ -7,10 +7,10 @@ public class IndiceCode : MonoBehaviour
     [SerializeField] private List<GameObject> _spawnPointIndices = new List<GameObject>();
     [SerializeField] private GameObject _noteCode;
 
-    private KeyCodeNumber _keyCodeNumber;
+    private KeyCodeChest _keyCodeChest;
     public void Start()
     {
-        _keyCodeNumber = GetComponent<KeyCodeNumber>();
+        _keyCodeChest = GetComponent<KeyCodeChest>();
         
         for (int i = 0;  i < 10; i++)
         {
@@ -19,11 +19,11 @@ public class IndiceCode : MonoBehaviour
     }
     public void PlaceIndice()
     {
-        for (int j = 0; j < _keyCodeNumber._keyCode.Count; j++)
+        for (int j = 0; j < _keyCodeChest._keyCode.Count; j++)
         {
             for (int i = 0; i < _indicesList.Count; i++)
             {
-                if (_keyCodeNumber._keyCode[j] == i)
+                if (_keyCodeChest._keyCode[j] == i)
                 {
                     _indicesList[i].SetActive(true);
                     _indicesList[i].transform.position = _spawnPointIndices[j].transform.position;
