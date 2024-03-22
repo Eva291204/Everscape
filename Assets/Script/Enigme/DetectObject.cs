fc69b42454b0c300a7d627ee60697000ff741029
Eva291204 <eva.fricaud@e-artsup.net>
2024-03-22 14:42:40 +0100

    début lvl6

--- Assets/Script/Enigme/DetectObject.cs
+++ Assets/Script/Enigme/DetectObject.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class DetectObject : MonoBehaviour
 {
+    [SerializeField] private bool _door2;
     public void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("PushItem"))
         {
             GameManager.Instance.NumberDetectObject++;
             GameManager.Instance.OpenDoor();
+
+            if (_door2)
+            {
+                GameManager.Instance.OpenDoor2();
+            }
         }
     }
 }
